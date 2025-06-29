3222ce196a1e652a44643cdf599f1f0310bc7fdf
lofmty <lofmty@gmail.com>
2025-06-29 07:02:47 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,44 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import {useEffect, useState} from "react"
+import styles from "./page.module.css"
+import WebApp from "@twa-dev/sdk"
+
+interface UserData {
+    id: number
+    first_name: string
+    last_name?: string
+    username?: string
+    language_code: string
+    is_premiuim?: boolean
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [userData, setuserData] = useState<UserData | null>(null)
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    useEffect(() => {
+        if (WebApp.initDataUnsafe.user) {
+            setuserData(WebApp.initDataUnsafe.user as UserData)
+        }
+    }, [])
+
+    return (
+        <div className={styles.page}>
+            <h1>TON-TRON</h1>
+            {userData ? (
+                <div>
+                    <p>ID: {userData.id}</p>
+                    <p>First Name: {userData.first_name}</p>
+                    <p>Last Name: {userData.last_name}</p>
+                    <p>Premium: {userData.is_premiuim}</p>
+                    <p>Language: {userData.language_code}</p>
+                    <p>Username: {userData.username}</p>
+                </div>
+            ) : (
+                <div>
+                    <span>Loading user data...</span>
+                </div>
+            )}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
